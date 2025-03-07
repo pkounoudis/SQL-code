cfe14c30675d5a12121f3e49b95feef19080419c
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-03-07 21:13:48 +0200

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -5,3 +5,12 @@ ALTER TABLE celebs
 ADD COLUMN twitter_handle TEXT; 
 
 SELECT * FROM celebs; 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (2, 'Beyonce Knowles', 42); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (3, 'Jeremy Lin', 35); 
+
+INSERT INTO celebs (id, name, age) 
+VALUES (4, 'Taylor Swift', 33); 
