aa0b56ecdfabffa0468c01af58c9c0f032c49a96
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-04-03 13:00:13 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -50,3 +50,6 @@ AND PostalCode > 12000;
 SELECT *
 FROM Customers
 WHERE Country = 'Germany' OR Country = 'Spain';
+
+SELECT * FROM Customers
+WHERE City = 'Berlin' OR CustomerName LIKE 'G%' OR Country = 'Norway';
