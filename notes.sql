d2cd1d8fa6d397e848e02cb9992473486ce3e143
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-08-05 13:50:34 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -161,3 +161,7 @@ FROM Customers;
 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+
+SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
+FROM Orders
+INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
