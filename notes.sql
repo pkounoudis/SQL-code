7d2012646755ecf5d603f0946564152825b2553b
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-08-11 18:24:48 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -165,3 +165,7 @@ INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+
+SELECT ProductID, ProductName, CategoryName
+FROM Products
+INNER JOIN Categories ON Products.CategoryID = Categories.CategoryID;
