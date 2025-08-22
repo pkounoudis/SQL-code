e3469d75cb19c4d0f50a6c8914485a0328cbad4c
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-08-22 13:11:06 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -174,3 +174,8 @@ SELECT column_name(s)
 FROM table1
 LEFT JOIN table2
 ON table1.column_name = table2.column_name;
+
+SELECT column_name(s)
+FROM table1
+RIGHT JOIN table2
+ON table1.column_name = table2.column_name;
