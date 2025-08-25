4299c1dbafe827b058c4da39b17456df6a1a0b35
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-08-25 22:29:53 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -179,3 +179,9 @@ SELECT column_name(s)
 FROM table1
 RIGHT JOIN table2
 ON table1.column_name = table2.column_name;
+
+SELECT column_name(s)
+FROM table1
+FULL OUTER JOIN table2
+ON table1.column_name = table2.column_name
+WHERE condition;
