fb9a68231c64b35fbe703114bb14a713dd346f44
Panos Kounoudis <39673318+pkounoudis@users.noreply.github.com>
2025-10-07 20:52:11 +0300

    Update notes.sql

--- notes.sql
+++ notes.sql
@@ -185,3 +185,9 @@ FROM table1
 FULL OUTER JOIN table2
 ON table1.column_name = table2.column_name
 WHERE condition;
+
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
